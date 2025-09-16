39e86456de525a30f79b1f9ad9bcaeb03023358c
Lenhard <149786490+lenhard01@users.noreply.github.com>
2025-09-17 04:24:36 +0800

    chore(2015): make function for part 1 and part 2

--- 2015/day01/main.py
+++ 2015/day01/main.py
@@ -1,3 +1,17 @@
 def parse_input(path: str) -> list:
     with open(path) as f:
         return [line.strip() for line in f]
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = parse_input("input.txt")
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
