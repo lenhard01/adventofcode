8b619d72e9ce9ae8b780f884b889f24e8624d6b5
Lenhard <149786490+lenhard01@users.noreply.github.com>
2025-09-18 04:14:29 +0800

    feat(2015): implement Day 1 part 1 and fix input parsing

--- 2015/day01/main.py
+++ 2015/day01/main.py
@@ -1,10 +1,17 @@
 def parse_input(path: str) -> list:
     with open(path) as f:
-        return [line.strip() for line in f]
+        return f.read().strip()
 
 
-def part1(data):
-    pass
+def part1(data: list[str]) -> int:
+    floor: int = 0
+
+    for char in data:
+        if char == "(":
+            floor += 1
+        else:
+            floor -= 1
+    return floor
 
 
 def part2(data):
@@ -12,6 +19,6 @@ def part2(data):
 
 
 if __name__ == "__main__":
-    data = parse_input("input.txt")
+    data = parse_input("2015/day01/input.txt")
     print("Part 1:", part1(data))
     print("Part 2:", part2(data))
